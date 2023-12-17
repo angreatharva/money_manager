423dd6d331111a4f5e115fc30508c727c29c8d45
Atharva Angre <angreatharva08@gmail.com>
2023-12-17 21:26:17 +0530

    Money_Manager

---
+++ lib/static.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+const Color PrimaryColor = Color(0xff87CF0E);
+
+const MaterialColor PrimaryMaterialColor = MaterialColor(
+  0xff87CF0E,
+  <int, Color>{
+    50: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .1,
+    ),
+    100: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .2,
+    ),
+    200: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .3,
+    ),
+    300: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .4,
+    ),
+    400: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .5,
+    ),
+    500: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .6,
+    ),
+    600: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .7,
+    ),
+    700: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .8,
+    ),
+    800: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .9,
+    ),
+    900: Color.fromRGBO(
+      135,
+      207,
+      14,
+      1,
+    ),
+  },
+);
---
+++ lib/theme.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+
+const Color PrimaryColor = Color(0xff87CF0E);
+
+const MaterialColor PrimaryMaterialColor = MaterialColor(
+  0xff87CF0E,
+  <int, Color>{
+    50: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .1,
+    ),
+    100: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .2,
+    ),
+    200: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .3,
+    ),
+    300: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .4,
+    ),
+    400: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .5,
+    ),
+    500: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .6,
+    ),
+    600: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .7,
+    ),
+    700: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .8,
+    ),
+    800: Color.fromRGBO(
+      135,
+      207,
+      14,
+      .9,
+    ),
+    900: Color.fromRGBO(
+      135,
+      207,
+      14,
+      1,
+    ),
+  },
+);
+
+ThemeData myTheme = ThemeData(
+  fontFamily: "customFont",
+  primaryColor: PrimaryColor,
+  primarySwatch: PrimaryMaterialColor,
+  appBarTheme: AppBarTheme(
+    titleTextStyle: TextStyle(
+      fontSize: 20.0,
+      fontFamily: "customFont",
+      fontWeight: FontWeight.w600,
+    ),
+  ),
+);
